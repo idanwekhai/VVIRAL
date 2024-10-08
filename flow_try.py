7d84c7ce8ea1ed2d983117ad1dc0e07a17f07012
Idanwekhai <kelvinidanwekhai@gmail.com>
2024-10-08 10:52:09 -0400

    Removed mlflow and extra pycorn files

--- flow_try.py
+++
@@ -1,8 +0,0 @@
-import mlflow
-
-remote_server_uri = "http://127.0.0.1:5000"  # set to your server URI
-mlflow.set_tracking_uri(remote_server_uri)
-mlflow.set_experiment("/my-experiment")
-with mlflow.start_run():
-    mlflow.log_param("a", 1)
-    mlflow.log_metric("b", 2)
